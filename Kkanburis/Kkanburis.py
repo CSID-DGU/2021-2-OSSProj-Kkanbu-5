79b6013ff11462bf4ebf4c3af74502c0cec5844d
jo-soobin <jsb802@naver.com>
2021-11-19 12:33:05 +0900

    set level speed

--- Kkanburis/Kkanburis.py
+++ Kkanburis/Kkanburis.py
@@ -1568,11 +1568,16 @@ while not done:
             elif event.type == USEREVENT:   # USEREVENT: 사용자가 임의로 설정하는 이벤트
                 # Set speed
                 if not game_over:
-                    keys_pressed = pygame.key.get_pressed()
-                    if keys_pressed[K_DOWN]:
-                        pygame.time.set_timer(pygame.USEREVENT, framerate * 1)
+                    if level <1:
+                        pygame.time.set_timer(pygame.USEREVENT, framerate * 15) #450
+                    elif level < 3:
+                        pygame.time.set_timer(pygame.USEREVENT, framerate * 15 - 50) #400
+                    elif level < 6:
+                        pygame.time.set_timer(pygame.USEREVENT, framerate * 10 + 50) #350
+                    elif level < 10:
+                        pygame.time.set_timer(pygame.USEREVENT, framerate * 10) #300
                     else:
-                        pygame.time.set_timer(pygame.USEREVENT, framerate * 20)
+                        pygame.time.set_timer(pygame.USEREVENT, framerate * 8 + 10) #250
 
                 # Draw a mino
                 draw_mino(dx, dy, mino, rotation)
