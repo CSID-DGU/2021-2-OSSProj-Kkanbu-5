d0576dd41bec8720b78fc8506f076bb59dedde73
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2021-12-04 05:58:31 +0900

    Modify Function Name

--- Kkanburis/database.py
+++ Kkanburis/database.py
@@ -36,8 +36,8 @@ def add_id(id_text):
 
 # 회원가입 시 사용자 등록을 위해 ID에 해당하는 비밀번호 추가
 def add_pw(id_text, pw_text):
-    hashed_pw = bcrypt.hashpw(pw_text.encode('utf-8'), bcrypt.gensalt())#비밀번호를 encoding해서 type를 byte로 바꿔서 hashpw함수에 넣기
-    decode_hash_pw = hashed_pw.decode('utf-8') #비밀번호 확인할 때는 str값으로 받아 매칭해서 비번을 데베에 저장할 때 decoding 해야함
+    hashed_pw = bcrypt.hashpw(pw_text.encode('utf-8'), bcrypt.gensalt())   # 비밀번호를 encoding해서 type를 byte로 바꿔서 hashpw 함수에 넣기
+    decode_hash_pw = hashed_pw.decode('utf-8')   # 비밀번호 확인할 때는 str값으로 받아 매칭해서 비번을 데베에 저장할 때 decoding 해야함
     curs = database.cursor()
     sql = "UPDATE users SET user_pw= %s WHERE user_id=%s"
     curs.execute(sql,(decode_hash_pw,id_text))
@@ -46,27 +46,6 @@ def add_pw(id_text, pw_text):
     print(decode_hash_pw)
 
     curs.close()
-
-# def add_pw(id_text, pw_text):
-#     #회원가입시 초기 아이템 수는 0으로 설정
-#     #추가하기
-#     initial_earthquake = 0
-#     initial_light  = 0
-#     initial_tnt = 0
-#     initial_gold = 0
-#     hashed_pw = bcrypt.hashpw(pw_text.encode('utf-8'), bcrypt.gensalt())#비밀번호를 encoding해서 type를 byte로 바꿔서 hashpw함수에 넣기
-#     decode_hash_pw = hashed_pw.decode('utf-8') #비밀번호 확인할 때는 str값으로 받아 매칭해서 비번을 데베에 저장할 때 decoding 해야함
-#     curs = database.cursor()
-#     sql = "UPDATE users SET user_pw= %s WHERE user_id=%s"
-#     curs.execute(sql,(decode_hash_pw,id_text))
-#     database.commit()
-#     print(hashed_pw)
-#     print(decode_hash_pw)
-#     curs = database.cursor()
-#     sql = "UPDATE users SET user_earthquake= %s, user_light= %s, user_tnt= %s, user_gold= %s WHERE user_id=%s"
-#     curs.execute(sql, (initial_earthquake,initial_light, initial_tnt, initial_gold, id_text))
-#     database.commit()
-#     curs.close()
 
 # 입력받은 아이디가 데이터베이스에 있는지 확인, 아이디와 비밀번호가 일치하는지 확인
 # 이 부분 보완 필요
@@ -89,7 +68,7 @@ def id_info(id_text):
     return user_id
 
 # 사용자의 점수 기록이 존재하는지 확인
-def exists_score(id_text):
+def exist_score(id_text):
     curs = database.cursor()
     sql = "SELECT * FROM sigle_rank WHERE user_id = %s "
     curs.execute(sql, id_text)
@@ -105,7 +84,7 @@ def add_score(game_status, id_text, score):
     # score 기록이 없는 사용자는 추가하고, score 기록이 있는 사용자는 등록된 score와 게임에서 얻은 score 중 높은 것으로 update 하기
     # score 테이블에서 id_text에 해당하는 사용자 데이터 있는지 확인
     if exist_id(id_text):   # 아이디가 존재하고
-        if exists_score(id_text):   # 아이디에 해당하는 사용자 점수 기록이 있는 경우 - 최고 기록으로 업데이트
+        if exist_score(id_text):   # 아이디에 해당하는 사용자 점수 기록이 있는 경우 - 최고 기록으로 업데이트
             curs = database.cursor()
             sql = "SELECT score from single_rank where user_id = %s"
             curs.execute(sql, id_text)
